fcda26eb41cec30768291b75e8190531ff1a974c
Michael Oeller <michaeloeller@gmx.at>
2018-06-26 19:46:15 +0200

    added request handling

--- MUS_Project/AWSLambda1/Function.cs
+++ MUS_Project/AWSLambda1/Function.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -12,16 +15,53 @@ namespace AWSLambda1
 {
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        private static int counter = 0;
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            var logger = context.Logger;
+
+            switch (input.Request)
+            {
+                case LaunchRequest launchRequest: return HandleLaunch(launchRequest, logger);
+                case IntentRequest intentRequest: return HandleIntent(intentRequest, logger);
+            }
+
+            throw new NotImplementedException("Unknown request type.");
+        }
+
+        private SkillResponse HandleLaunch(LaunchRequest launchRequest, ILambdaLogger logger)
+        {
+            logger.LogLine($"LaunchRequest made");
+            var response = ResponseBuilder.Tell(new PlainTextOutputSpeech()
+            {
+                Text = $"Welcome! You can ask me to say hello world. {counter}"
+            });
+
+            return response;
+        }
+
+        private SkillResponse HandleIntent(IntentRequest intentRequest, ILambdaLogger logger)
+        {
+            logger.LogLine($"IntentRequest {intentRequest.Intent.Name} made");
+
+            var responseSpeech = "Hello world";
+
+            if (intentRequest.Intent.Slots.TryGetValue("City", out var citySlot))
+            {
+                if (!string.IsNullOrEmpty(citySlot.Value))
+                {
+                    responseSpeech += $" from {citySlot.Value}";
+                }
+            }
+
+            responseSpeech += "!";
+
+            var response = ResponseBuilder.Tell(new PlainTextOutputSpeech()
+            {
+                Text = responseSpeech
+            });
+
+            return response;
         }
     }
 }
