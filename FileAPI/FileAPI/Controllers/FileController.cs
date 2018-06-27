2f5b31a2d8741e65adbfba78b853623ee5b78697
Oeller Michael <Michael.Oeller@snt-is.com>
2018-06-27 18:08:10 +0200

    added fileAPI

---
+++ FileAPI/FileAPI/Controllers/FileController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FileHandler.Implementation;
+using FileHandler.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MUS.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        private static IFileHandler fh = new FileHandlerImpl();
+
+        public FileController(IFileHandler fh) {
+            fh = new FileHandlerImpl();
+        }
+
+        [Route("File")]
+        //returning file to client
+        [HttpGet]
+        public HttpResponseMessage Generate()
+        {
+            var stream = new MemoryStream();
+            // processing the stream.
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new ByteArrayContent(stream.ToArray())
+            };
+            result.Content.Headers.ContentDisposition =
+                new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "program.cs"
+            };
+            result.Content.Headers.ContentType =
+                new MediaTypeHeaderValue("application/octet-stream");
+
+            return result;
+        }
+
+        // POST api/values
+        [Route("CreateFile")]
+        [HttpPost]
+        public bool Post([FromBody]string name)
+        {
+            return fh.CreateFile(name);
+        }
+
+
+        // POST api/values
+        [Route("CreateClass")]
+        [HttpPost]
+        public void Post([FromBody]string[] value)
+        {
+
+        }
+
+         // POST api/values
+        [Route("CreateWhile")]
+        [HttpPost]
+        public void CreateWhile([FromBody]string[] value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FileAPI/FileAPI/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using FileHandler.Implementation;
+using FileHandler.Interface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace FileAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(hostingEnvironment.ContentRootPath)
+               .AddJsonFile("program.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        private IHostingEnvironment _hostingEnvironment;
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            var physicalProvider = _hostingEnvironment.ContentRootFileProvider;
+            var embeddedProvider = new EmbeddedFileProvider(Assembly.GetEntryAssembly());
+            var compositeProvider = new CompositeFileProvider(physicalProvider, embeddedProvider);
+
+            // choose one provider to use for the app and register it
+            //services.AddSingleton<IFileProvider>(physicalProvider);
+            //services.AddSingleton<IFileProvider>(embeddedProvider);
+            services.AddSingleton<IFileProvider>(compositeProvider);
+            services.AddScoped<IFileHandler, FileHandlerImpl>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "REST API",
+                    Description = "REST API for SVE project",
+                    TermsOfService = "None",
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "REST API v1");
+            });
+        }
+    }
+}
