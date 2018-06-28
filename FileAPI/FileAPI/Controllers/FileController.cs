9fe7c65b239802f781c21ebdf28097d72d35fdfb
Kinni012 <AlexanderKinast@gmx.at>
2018-06-28 10:41:10 +0200

    work in progress

--- FileAPI/FileAPI/Controllers/FileController.cs
+++ FileAPI/FileAPI/Controllers/FileController.cs
@@ -12,108 +12,95 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MUS.API.Controllers
 {
-    [Route("api/[controller]")]
-    public class FileController : Controller
+  [Route("api/[controller]")]
+  public class FileController : Controller
+  {
+    private static IFileHandler fh = new FileHandlerImpl();
+
+    static FileController()
     {
-        private static IFileHandler fh = new FileHandlerImpl();
-
-        public FileController(IFileHandler fh)
-        {
-            fh = new FileHandlerImpl();
-        }
-
-        [Route("File")]
-        //returning file to client
-        [HttpGet]
-        public HttpResponseMessage Generate()
-        {
-            var stream = new MemoryStream();
-            // processing the stream.
-
-            var result = new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new ByteArrayContent(stream.ToArray())
-            };
-            result.Content.Headers.ContentDisposition =
-                new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment")
-                {
-                    FileName = "program.cs"
-                };
-            result.Content.Headers.ContentType =
-                new MediaTypeHeaderValue("application/octet-stream");
-
-            return result;
-        }
-
-        // POST api/values
-        [Route("CreateFile")]
-        [HttpPost]
-        public bool Post([FromBody]string name)
-        {
-            return true;
-        }
-
-
-        // POST api/values
-        [Route("CreateClass")]
-        [HttpPost]
-        public void Post([FromBody]string[] value)
-        {
-
-        }
-
-        // POST api/values
-        [Route("CreateIf")]
-        [HttpPost]
-        public void CreateIf([FromBody] dynamic data)
-        {
-            string varName = data.varName;
-            string compareType = data.varName;
-            string number = data.varName;
-
-            List<string> l = new List<string>();
-
-
-
-        }
-
-
-
-        // POST api/values
-        [Route("SetColumn")]
-        [HttpPost]
-        public bool SetColumn([FromBody] dynamic data)
-        {
-            fh.currentPositionInLine = data.columnNumber;
-            return true;
-        }
-
-
-        // POST api/values
-        [Route("SetRow")]
-        [HttpPost]
-        public bool SetRow([FromBody] dynamic data)
-        {
-            fh.currentLine = data.lineNumber;
-            return true;
-        }
-
-
-        // POST api/values
-        [Route("DeleteLine")]
-        [HttpPost]
-        public string DeleteLine([FromBody] dynamic data)
-        {
-            return fh.DeleteRange(data.lineNr, 1);
-        }
-
-
-        // POST api/values
-        [Route("ReadFile")]
-        [HttpPost]
-        public string ReadFile()
-        {
-            return fh.ReadFile();
-        }
+      CreateFile();
     }
+
+    public FileController(IFileHandler fh)
+    {
+    }
+    
+    private static void CreateFile()
+    {
+      List<string> l = new List<string>();
+      l.Add("using System;");
+      l.Add("using System.Collections.Generic;");
+      l.Add("using System.Linq;");
+      l.Add("using System.Text;");
+      l.Add("using System.Threading.Tasks;");
+      l.Add("");
+      l.Add("namespace ConsoleApp1");
+      l.Add("{");
+      l.Add("  class program");
+      l.Add("  {");
+      l.Add("    static void Main(string[] args)");
+      l.Add("    {");
+      l.Add("      ");
+      l.Add("    {");
+      l.Add("  }");
+      l.Add("}");
+      fh.AppendToFile(l);
+    }
+
+
+    // POST api/values
+    [Route("CreateIf")]
+    [HttpPost]
+    public void CreateIf([FromBody] dynamic data)
+    {
+      string varName = data.varName;
+      string compareType = data.varName;
+      string number = data.varName;
+
+      List<string> l = new List<string>();
+
+
+
+    }
+
+
+
+    // POST api/values
+    [Route("SetColumn")]
+    [HttpPost]
+    public bool SetColumn([FromBody] dynamic data)
+    {
+      fh.currentPositionInLine = data.columnNumber;
+      return true;
+    }
+
+
+    // POST api/values
+    [Route("SetRow")]
+    [HttpPost]
+    public bool SetRow([FromBody] dynamic data)
+    {
+      fh.currentLine = data.lineNumber;
+      return true;
+    }
+
+
+    // POST api/values
+    [Route("DeleteLine")]
+    [HttpPost]
+    public string DeleteLine([FromBody] dynamic lineNr)
+    {
+      return fh.DeleteRange(lineNr, 1);
+    }
+
+
+    // POST api/values
+    [Route("ReadFile")]
+    [HttpPost]
+    public string ReadFile()
+    {
+      return fh.ReadFile();
+    }
+  }
 }
